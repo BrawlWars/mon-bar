f15051b344b6d2fff6e7f292ab3f245de40d57e4
BrawlWars <contact.brawlwars@gmail.com>
2026-08-18 17:36:51 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mon-bar-v8";
+const CACHE_NAME = "mon-bar-v12";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-180.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
